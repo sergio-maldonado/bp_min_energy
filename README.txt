7acb7ac0d5d0dc737b48d5fa80ebc770328e0c4b
sergio-maldonado <53574986+sergio-maldonado@users.noreply.github.com>
2019-11-15 15:09:39 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+This folder contains the following files:
+
+1. 'mathematica_aided_work.nb' is a Wolfram Mathematica notebook (written in version 11.2.0.0) that supplements some of the work presented in sections 3 and 4. This file can be used to reproduce any solution given in the paper. More details are given in the notebook.
+
+2. 'value_functional.m' is a Matlab code (written in version R2017b) used to estimate numerically the functional J[h(x)] and maximum local bed slope for any given curve (e.g. solutions produced above or measured/digitised profiles). Additional details are provided in the code.
+
+3. 'data_for_tables.xlsx' summarises the results of using the two codes above for all profiles considered in this study. These data feed into the tables of the paper. Additional notes can be found in the spreadsheet.
+
+4. Three files with extension *.sfit are sessions for the 'cftool' toolbox of Matlab (version 2017b) that give the results of the best-fits to the measured profiles.
+
+
+
+Sergio Maldonado
---
+++ value_functional.m
@@ -0,0 +1,74 @@
+% Computes the value of the functional J[h(x)] for a given curve
+% and also maximum local bed slope found in the profile
+%
+% Sergio Maldonado 2019
+% written in Matlab 2017b
+%
+% supplementary code to:
+% Maldonado, S. (under review) Do beach profiles under non-breaking
+% waves minimise energy dissipation?
+%
+% NOTES:
+% -F[h] may be estimated analytically for some curves (e.g. a line), but we
+% use the same numerical method below in all cases for consistency.
+%
+% -central finite differences (FDs) are preferred for measured profiles,
+% while upwind FDs are used for all other curves. The criterion being the
+% smoothness of the curve.
+
+clear variables
+clc
+close all
+
+%input data ==============================================
+
+%beach profile:
+dire  = '/some/folder/here/';
+file = 'some_profile.dat';
+
+n = 2;  % n_tau
+
+%input data ==============================================
+
+cd(dire)
+ 
+ne = -3*(n + 1)/4;
+
+pr = importdata(file);
+% pr = pr';
+
+x = pr(:,1);
+h = pr(:,2);
+
+% central scheme:
+% dhdx = (h(3:end) - h(1:end-2))./(x(3:end) - x(1:end-2));
+% dhdx = [dhdx(1) ; dhdx ; dhdx(end)];
+
+% upwind scheme:
+dhdx = (h(2:end) - h(1:end-1))./(x(2:end) - x(1:end-1));
+dhdx = [dhdx ; dhdx(end)];
+
+L = (h.^ne).*sqrt(1 + dhdx.^2);
+
+% functional is
+F = trapz(x,L);
+
+disp('The value of the functional is:')
+disp(F)
+
+disp('The max bed slope is:')
+disp(max(dhdx))
+
+% plots for reference:
+
+% figure
+% plot(x,h,'-k')
+% title('beach profile')
+% xlabel('x (m)')
+% ylabel('h (m)')
+% 
+% figure
+% plot(x,dhdx)
+% title('slope of the profile dh/dx')
+% xlabel('x (m)')
+% ylabel('dh/dx')
